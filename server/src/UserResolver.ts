a1379adc1da7af5be9beeea53ccbfda128082176
Christos Paschalidis <paschalidi@outlook.com>
2019-10-16 09:54:16 +0200

    eslints
    adds middleware for isAuth routes

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,8 +1,18 @@
-import {Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver} from 'type-graphql'
-import {compare, hash} from "bcryptjs";
-import {User} from "./entity/User";
-import {sign} from 'jsonwebtoken'
-import {MyContext} from "./MyContext";
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware
+} from "type-graphql";
+import { compare, hash } from "bcryptjs";
+import { User } from "./entity/User";
+import { MyContext } from "./MyContext";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { isAuth } from "./isAuth";
 
 @ObjectType()
 class LoginResponse {
@@ -10,58 +20,57 @@ class LoginResponse {
   accessToken: string;
 }
 
-
 @Resolver()
-export class UserResolver{
-    @Query(() => String)
-    hello() {
-        return 'HI!'
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "HI!";
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Invalid Login");
+    }
+    const isValid = await compare(password, user.password);
+
+    if (!isValid) {
+      throw new Error("Invalid Login");
     }
 
-    @Query(() => [User])
-    users() {
-        return User.find()
+    res.cookie("jid", createRefreshToken(user), { httpOnly: true });
+    return { accessToken: createAccessToken(user) };
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+    try {
+      await User.insert({ email, password: hashedPassword });
+    } catch (e) {
+      return false;
     }
-
-    @Mutation(()=> LoginResponse)
-    async login(
-        @Arg('email') email: string,
-        @Arg('password') password:string,
-        @Ctx() { res }: MyContext
-    ): Promise<LoginResponse>{
-
-        const user = await User.findOne({where:{email}})
-
-        if(!user){
-            throw new Error('Invalid Login')
-        }
-        const isValid = await compare(password, user.password)
-
-        if(!isValid){
-            throw new Error('Invalid Login')
-        }
-
-        res.cookie(
-            'jid',
-                    sign({userId: user.id}, 'some other random String!', {expiresIn:'7d'}),
-        {httpOnly:true}
-        )
-        return {accessToken: sign({userId: user.id}, 'randomString!',{expiresIn:'15m'})}
-    }
-
-
-    @Mutation(()=> Boolean)
-    async register(
-        @Arg('email') email: string,
-        @Arg('password') password:string
-    ){
-        const hashedPassword = await hash(password, 12)
-        try {
-            await User.insert({email, password: hashedPassword})
-        }
-        catch (e) {
-            return false
-        }
-        return true
-    }
+    return true;
+  }
 }
